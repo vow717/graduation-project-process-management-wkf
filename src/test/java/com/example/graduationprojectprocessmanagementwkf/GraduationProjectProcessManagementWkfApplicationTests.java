d8aae69c046770b4d72eedd0193a90a8e51a6509
vow717 <147890905+vow717@users.noreply.github.com>
2025-03-17 22:31:08 +0800

    test

--- src/test/java/com/example/graduationprojectprocessmanagementwkf/GraduationProjectProcessManagementWkfApplicationTests.java
+++ src/test/java/com/example/graduationprojectprocessmanagementwkf/GraduationProjectProcessManagementWkfApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.graduationprojectprocessmanagementwkf;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "spring.sql.init.mode=never")
+@SpringBootTest
 class GraduationProjectProcessManagementWkfApplicationTests {
 
     @Test
